905a36beb85cbf6432c64ef6d83b8f4b2432f0dc
Juan <Juan.Paggi@sdc.com.ar>
2020-01-14 10:47:31 -0300

    Desarrollo de panel

--- Pintura/src/app/header/header.component.ts
+++ Pintura/src/app/header/header.component.ts
@@ -26,7 +26,6 @@ export class HeaderComponent implements OnInit {
 
   clickSalir(){
     this.usuariosSrv.setUserLoggedOut();
-    this.router.navigateByUrl(`/`);
     window.location.reload();
   }
 
